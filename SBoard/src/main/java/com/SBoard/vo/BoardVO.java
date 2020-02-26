fd86019e245eba584326385e8c3deaeae7b6abb9
doo9104 <poepoe117@naver.com>
2020-02-26 19:07:52 +0900

    BoardVO 테이블 컬럼 구조 반영

---
+++ SBoard/src/main/java/com/SBoard/vo/BoardVO.java
@@ -0,0 +1,19 @@
+package com.SBoard.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	
+	private Long bno; 
+	private String btitle;
+	private String bcontent;
+	private String bwriter;
+	private long bhit;
+	private long brec_up;
+	private long brec_dn;
+	private Date bregdate;
+	private Date bupdateregdate;
+}
